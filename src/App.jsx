8f98906e370f6f8bb6b0ac93053f91fb5f7086e2
Nastia Kartseva <danetzachem@gmail.com>
2022-09-28 15:40:15 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styles from './styles/App.module.css';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Cоздание анкеты</h1>
+      <div className={styles.form}></div>
+    </div>
+  );
+}
+
+export default App;
