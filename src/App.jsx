7e8456c0f09b2bac175b67c719954b64c373693d
Nastia Kartseva <danetzachem@gmail.com>
2022-09-29 15:01:03 +0300

    updated styles

--- src/App.jsx
+++ src/App.jsx
@@ -40,11 +40,11 @@ class App extends React.Component {
             <FormInput name="name" label="Имя" placeholder="Александра" />
             <FormInput name="surname" label="Фамилия" placeholder="Александрова" />
             <FormInput type="date" name="birthday" label="Дата рождения" placeholder="01.01.1991" />
-            <FormInput type="tel" name="phone" label="Телефон" placeholder="+7 (999) 999 99 99" />
+            <FormInput type="tel" name="phone" label="Телефон" placeholder="+79992223311" />
             <FormInput name="website" label="Сайт" placeholder="https://github.com/leteli/react-form" />
-            <MultiLineInput name="personal" label="О себе" placeholder="Я начинающий разработчик, ..." />
-            <MultiLineInput name="tech-stack" label="Стек технологий" placeholder="JS, CSS, React, ..." />
-            <MultiLineInput name="last-project" label="Описание последнего проекта" placeholder="Я реализовала" />
+            <MultiLineInput name="personal" label="О себе" placeholder="Я начинающий разработчик..." />
+            <MultiLineInput name="tech-stack" label="Стек технологий" placeholder="JS, CSS, React..." />
+            <MultiLineInput name="last-project" label="Описание последнего проекта" placeholder="Я реализовала..." />
             <ButtonGroup />
           </form>
         </div>
