7bbb197a2c21b9e7922a6e5de527e651034304c5
Nastia Kartseva <danetzachem@gmail.com>
2022-09-30 20:43:31 +0300

    changed to functional components

--- src/FormInput.jsx
+++ src/FormInput.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import styles from './styles/FormInput.module.css';
 
-class FormInput extends React.Component {
-  render() {
-    return (
-      <>
-        <label htmlFor={this.props.name}>{this.props.label}</label>
-        <input
-          className={styles.input}
-          type={this.props.type ?? "text"}
-          id={this.props.name}
-          name={this.props.name}
-          placeholder={this.props.placeholder}
-        />
-      </>
-    );
-  }
-}
+const FormInput = (props) => {
+  return (
+    <>
+      <label htmlFor={props.name}>{props.label}</label>
+      <input
+        className={styles.input}
+        type={props.type ?? "text"}
+        id={props.name}
+        name={props.name}
+        placeholder={props.placeholder}
+      />
+    </>
+  );
+};
 
 export default FormInput;
--- src/MultiLineInput.jsx
+++ src/MultiLineInput.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import styles from './styles/FormInput.module.css';
 
-class MultiLineInput extends React.Component {
-  render() {
-    return (
-      <>
-        <label htmlFor={this.props.name}>{this.props.label}</label>
-        <textarea
-          className={styles.input}
-          rows="7"
-          name={this.props.name}
-          id={this.props.name}
-          placeholder={this.props.placeholder}
-        />
-      </>
-    );
-  }
-}
+const MultiLineInput = (props) => {
+  return (
+    <>
+      <label htmlFor={props.name}>{props.label}</label>
+      <textarea
+        className={styles.input}
+        rows="7"
+        name={props.name}
+        id={props.name}
+        placeholder={props.placeholder}
+      />
+    </>
+  );
+};
 
 export default MultiLineInput;
