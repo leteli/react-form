fcb1e07700459d71d0e2e5c626ada27d74c54040
Nastia Kartseva <danetzachem@gmail.com>
2022-10-03 02:09:59 +0300

    add styling for invalid fields

--- src/FormInput.jsx
+++ src/FormInput.jsx
@@ -4,17 +4,17 @@ import styles from './styles/FormInput.module.css';
 const FormInput = (props) => {
   return (
     <>
-      <label htmlFor={props.name}>{props.label}</label>
+      <label className={styles.label} htmlFor={props.name}>{props.label}</label>
       <input
         onChange={props.inputHandler}
         value={props.state.value}
-        className={styles.input}
+        className={props.state.isValid === false ? styles.invalidInput : styles.input}
         type={props.type ?? "text"}
         id={props.name}
         name={props.name}
         placeholder={props.placeholder}
       />
-      { props.state.isValid === false ? <div>{ props.state.message }</div> : null }
+      { props.state.isValid === false ? <div className={styles.invalid}>{ props.state.message }</div> : null }
     </>
   );
 };
--- src/MultiLineInput.jsx
+++ src/MultiLineInput.jsx
@@ -5,17 +5,21 @@ import styles from './styles/FormInput.module.css';
 const MultiLineInput = (props) => {
   return (
     <>
-      <label htmlFor={props.name}>{props.label}</label>
+      <label className={styles.label} htmlFor={props.name}>{props.label}</label>
       <textarea
         onChange={props.inputHandler}
         value={props.state.value}
-        className={styles.input}
+        className={props.state.isValid === false ? styles.invalidInput : styles.input}
         rows="7"
         name={props.name}
         id={props.name}
         placeholder={props.placeholder}
       />
-      { props.state.isValid === null ? null: <div>{props.state.message}</div> }
+      { props.state.isValid === null ? null : 
+        <div className={props.state.isValid === false ? styles.invalid : null }>
+          {props.state.message}
+        </div>
+      }
     </>
   );
 };
