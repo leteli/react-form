3fcf0024e192a26db6465affbfbf10964be493b7
Nastia Kartseva <danetzachem@gmail.com>
2022-10-05 22:52:15 +0300

    styled Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,18 @@
 import { Link } from 'react-router-dom';
 import Layout from './Layout.jsx';
+import layout from '../styles/Layout.module.css';
+import button from '../styles/ButtonGroup.module.css';
+import styles from '../styles/Home.module.css';
 
 const Home = () => {  
   return (
     <Layout>
-      <h1>Анкета для разработчиков</h1>
-      <div>
-        <Link to="/form">Заполнить</Link>
-        <Link to="/info">Моя анкета</Link>
+      <h1 className={layout.title}>Анкета разработчика</h1>
+      <p className={styles.text}>Простое приложение для создания CV</p>
+      <div className={styles.btnWrapper}>
+        <button className={button.primary}>
+          <Link to="/form">Заполнить</Link>
+        </button>
       </div>
     </Layout>
   );
