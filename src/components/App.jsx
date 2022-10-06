4fa610dd2cb6c1259917035fbf1e0acddc181485
Nastia Kartseva <danetzachem@gmail.com>
2022-10-06 16:30:20 +0300

    Moved state in upper component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
+import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home.jsx';
 import Form from './Form.jsx';
 import FilledForm from './FilledForm.jsx';
 
 const App = () => {
+  const initialData = {value: '', isValid: null, message: null };
+  const inputNames = ['name', 'surname', 'birthday', 'phone', 'website', 'personal', 'techStack', 'lastProject'];
+  const initialState = {};
+  inputNames.forEach((name) => initialState[name] = { ...initialData });
+  const [values, setValues] = useState(initialState);
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/form" element={<Form />} />
-      <Route path="/info" element={<FilledForm />} />
+      <Route path="/form" element={<Form values={values} setValues={setValues}/>} />
+      <Route path="/info" element={<FilledForm state={values}/>} />
     </Routes>
   );
 };
