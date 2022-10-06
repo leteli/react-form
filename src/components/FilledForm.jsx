19874c75c75cf6f9f73d90aecacf7a503c700310
Nastia Kartseva <danetzachem@gmail.com>
2022-10-06 16:31:22 +0300

    Implement ui for not filled form

--- src/components/FilledForm.jsx
+++ src/components/FilledForm.jsx
@@ -1,20 +1,30 @@
-import styles from '../styles/FilledForm.module.css';
 import Layout from './Layout.jsx';
+import ReturnButton from './ReturnButton.jsx';
+import formStyles from '../styles/Form.module.css';
+import styles from '../styles/FilledForm.module.css';
 import layout from '../styles/Layout.module.css';
 
 const FilledForm = (props) => {
   const { name, surname, birthday, phone, website, personal, techStack, lastProject } = props.state;
   return (
     <Layout>
-      <h1 className={layout.title}>{`${name.value} ${surname.value}`}</h1>
-      <div>
-        <p className={styles.field}><span>Дата рождения:</span>{birthday.value}</p>
-        <p className={styles.field}><span>Телефон:</span>{phone.value}</p>
-        <p className={styles.field}><span>Сайт:</span>{website.value}</p>
-        <p className={styles.field}><span>О себе:</span>{personal.value}</p>
-        <p className={styles.field}><span>Стек технологий:</span>{techStack.value}</p>
-        <p className={styles.field}><span>Описание последнего проекта:</span>{lastProject.value}</p>
+      <div className={formStyles.contentHeader}>
+        <ReturnButton />
+        { name.value !== ''
+        ? <h1 className={layout.title}>{`${name.value} ${surname.value}`}</h1>
+        : <h1 className={layout.title}>Анкета не заполнена</h1>
+        }
       </div>
+      { name.value !== '' && (
+        <div>
+          <p className={styles.field}><span>Дата рождения:</span>{birthday.value}</p>
+          <p className={styles.field}><span>Телефон:</span>{phone.value}</p>
+          <p className={styles.field}><span>Сайт:</span>{website.value}</p>
+          <p className={styles.field}><span>О себе:</span>{personal.value}</p>
+          <p className={styles.field}><span>Стек технологий:</span>{techStack.value}</p>
+          <p className={styles.field}><span>Описание последнего проекта:</span>{lastProject.value}</p>
+        </div>
+      )}
     </Layout>
   );
 };
